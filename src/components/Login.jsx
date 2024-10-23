10f1b12b893adf87ccc797d7da23f686cac255d6
Alto0327 <aj.silvaf2003@gmail.com>
2024-10-23 00:01:13 -0400

    Styled Login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import SpotifyLogo from "../assets/Spotify_Primary_Logo_RGB_White.png"
+import "../styles/Login.css"
+
 
 const AUTH_URL =
   "https://accounts.spotify.com/authorize?client_id=fbae3b0191774f28a48d431355216faf&response_type=code&redirect_uri=http://localhost:5173/callback&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
 function Login() {
   
     return (
-    <>
-        <img src={SpotifyLogo} alt="Spotify Logo" width="200"/>
-        <button onClick={() => window.location = AUTH_URL}>Log into spotify</button>
-    </>
+    <div className='login'>
+        <img src={SpotifyLogo} alt="Spotify Logo" width="200" className='Logo'/>
+        <button onClick={() => window.location = AUTH_URL} className='btn'>Log in</button>
+    </div>
   );
 }
 
